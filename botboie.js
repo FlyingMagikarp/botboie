99c94f19f893463da44929470cb33bc7dc1eb263
Alvaro Fernandez <fernandez-alvaro@hotmail.com>
2017-11-22 15:57:17 +0100

    added memes

--- botboie.js
+++ botboie.js
@@ -1,24 +1,72 @@
 const Discord = require("discord.js");
 const client = new Discord.Client();
+var fs = require("fs");
 
 client.on("ready", () => {
   console.log("BotBoie.js is ready!");
 });
 
+// command prefix
+
+const prefix = "!";
 client.on("message", (message) => {
-  if(message.content.startsWith("!ping")) {
-    message.channel.send("pong!");
+  // ignore bot messages
+  if (!message.content.startsWith(prefix) || message.author.bot) return;
+  // events
+  if(message.content.startsWith(prefix+"ping")) {
+    send_text("pong!");
   }
-  if(message.content.startsWith("!cyka")){
-    message.channel.send("blyet!");
+  if(message.content.startsWith(prefix+"cyka")){
+    send_text("blyet!");
   }
-  if(message.content.startsWith("!lenny")){
-    message.channel.send("( ͡° ͜ʖ ͡°)");
+  if(message.content.startsWith(prefix+"no")){
+    send_text("ಠ_ಠ");
   }
-  if(message.content.startsWith("!no")){
-    message.channel.send("ಠ_ಠ");
+  if(message.content.startsWith(prefix+"vaporwoah")){
+    send_text("https://www.youtube.com/watch?v=24JJ1_A5HP0");
   }
-  
+  if(message.content.startsWith(prefix+"dab")){
+    var file = "memes/squidward_dab.jpg";
+    send_img(file);
+  }
+  if(message.content.startsWith(prefix+"meme")){
+    send_random_meme();
+  }
+  if(message.content.startsWith(prefix+"mlg")){
+    var file = "memes/mlgfrog.gif";
+    send_img(file);
+  }
+
+
+
+
+  // FUNCTIONS
+  function send_text(text){
+    message.channel.send(text);
+  }
+
+  function send_img(path){
+    message.channel.sendMessage({
+      file: path
+    });
+  }
+
+  function send_random_meme(){
+    var directory = "memes/";
+    var meme = "";
+    var memes = [];
+
+    fs.readdir(directory, function(err, memes){
+      maxVal = memes.length;
+      random = Math.floor(Math.random()*maxVal);
+      meme = directory+memes[random];
+
+      send_img(meme);
+    });
+  }
+
+
 });
+
 
 client.login("MzgyODQwNzU3Mjc2NjM5MjQz.DPbkBg.EH1EQDuzwWD2PUNsZZ3U3WGvd_w");
